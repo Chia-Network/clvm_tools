2fda6cf0c9baa60dd6c8b4f756c5dfd6f2a6ca35
Richard Kiss <him@richardkiss.com>
2019-08-05 13:28:36 -0700

    Add map and compile_qq_sexp.

--- bindings/stage_2/bindings.py
+++ bindings/stage_2/bindings.py
@@ -1,10 +1,10 @@
-from clvm import eval_f
+from clvm import eval_f, to_sexp_f
 
 from ..patch_eval_f import bind_eval_f, wrap_eval_f
 
 from .expand import expand_sexp, DEFAULT_MACRO_LOOKUP
 from .qa import qa_sexp
-from .qq import qq_sexp
+from .qq import compile_qq_sexp
 
 
 def do_expand_op(args, eval_f):
@@ -22,16 +22,23 @@ def do_qa_op(args, eval_f):
     return qa_sexp(args.first())
 
 
-def do_qq_op(args, eval_f):
+def do_compile_qq_op(args, eval_f):
     if len(args.as_python()) != 1:
         raise SyntaxError("require 1 argument to qq")
-    qquoted_sexp = qq_sexp(args.first())
-    r = eval_f(eval_f, qquoted_sexp, args.null())
-    return r
+    return compile_qq_sexp(args.first())
 
 
 def do_list(args, eval_f):
     return args
+
+
+def do_map(args, eval_f):
+    if len(args.as_python()) != 2:
+        raise SyntaxError("require 2 arguments to map")
+    r = []
+    for sexp in args.rest().first().as_iter():
+        r.append(eval_f(eval_f, args.first(), to_sexp_f([sexp])))
+    return to_sexp_f(r)
 
 
 def qa_transformer(sexp, env, eval_f):
@@ -42,7 +49,8 @@ BINDINGS = {
     "expand_op": do_expand_op,
     "list": do_list,
     "qa_op": do_qa_op,
-    "qq_op": do_qq_op,
+    "compile_qq_op": do_compile_qq_op,
+    "map": do_map,
 }
 
 
