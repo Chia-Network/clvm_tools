67fa0cb462a5056a713ed5571703112992f4e7aa
Richard Kiss <him@richardkiss.com>
2019-08-19 18:13:35 -0700

    Allow multiple interdependent macros.

--- stage_com/macros.py
+++ stage_com/macros.py
@@ -20,13 +20,13 @@ DEFAULT_MACRO_LOOKUP = None
 
 def build_default_macro_lookup(eval_f):
     run = binutils.assemble("(opt (com (f (a)) (r (a))))")
-    macro_lookup = to_sexp_f([])
+    global DEFAULT_MACRO_LOOKUP
     for macro_src in DEFAULT_MACROS_SRC:
         macro_sexp = binutils.assemble(macro_src)
-        env = macro_sexp.to((macro_sexp, macro_lookup))
+        env = macro_sexp.to((macro_sexp, DEFAULT_MACRO_LOOKUP))
         new_macro = eval_f(eval_f, run, env)
-        macro_lookup = new_macro.rest().first().cons(macro_lookup)
-    return macro_lookup
+        DEFAULT_MACRO_LOOKUP = new_macro.rest().first().cons(DEFAULT_MACRO_LOOKUP)
+    return DEFAULT_MACRO_LOOKUP
 
 
 def default_macro_lookup(eval_f):
