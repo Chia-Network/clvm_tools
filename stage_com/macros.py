daca9226b3636f7dffeb99bd6b1e22326e1f1296
Richard Kiss <him@richardkiss.com>
2019-08-20 11:01:38 -0700

    Fix a gross macro hack.

--- stage_com/macros.py
+++ stage_com/macros.py
@@ -28,13 +28,13 @@ DEFAULT_MACRO_LOOKUP = None
 
 
 def build_default_macro_lookup(eval_f):
-    run = binutils.assemble("(opt (com (f (a)) (r (a))))")
+    run = binutils.assemble("(e (com (f (a)) (r (a))) (a))")
     global DEFAULT_MACRO_LOOKUP
     for macro_src in DEFAULT_MACROS_SRC:
         macro_sexp = binutils.assemble(macro_src)
         env = macro_sexp.to((macro_sexp, DEFAULT_MACRO_LOOKUP))
         new_macro = eval_f(eval_f, run, env)
-        DEFAULT_MACRO_LOOKUP = new_macro.rest().first().cons(
+        DEFAULT_MACRO_LOOKUP = new_macro.cons(
             DEFAULT_MACRO_LOOKUP)
     return DEFAULT_MACRO_LOOKUP
 
