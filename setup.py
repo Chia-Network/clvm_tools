044953fc3b35919a4f75f84086781e38ed1a685f
Richard Kiss <him@richardkiss.com>
2020-01-28 12:58:18 -0800

    Use setuptools-scm.

--- setup.py
+++ setup.py
@@ -2,39 +2,37 @@
 
 from setuptools import setup
 
-from clvm_tools.version import version
-
 
 setup(
     name="clvm_tools",
-    version=version,
-    packages=[
-        "ir",
-        "clvm_tools",
-        "clvm_tools.setuptools",
-        "stage_2",
-    ],
+    packages=["ir", "clvm_tools", "clvm_tools.setuptools", "stage_2",],
     author="Chia Network, Inc.",
-
     entry_points={
-        'console_scripts':
-            [
-                'read_ir = clvm_tools.cmds:read_ir',
-                'opc = clvm_tools.cmds:opc',
-                'opd = clvm_tools.cmds:opd',
-                'run = clvm_tools.cmds:run',
-                'brun = clvm_tools.cmds:brun',
-            ],
-        },
+        "console_scripts": [
+            "read_ir = clvm_tools.cmds:read_ir",
+            "opc = clvm_tools.cmds:opc",
+            "opd = clvm_tools.cmds:opd",
+            "run = clvm_tools.cmds:run",
+            "brun = clvm_tools.cmds:brun",
+        ],
+    },
     author_email="kiss@chia.net",
+    install_requires=["setuptools_scm"],
+    setup_requires=["setuptools_scm"],
+    use_scm_version=True,
     url="https://github.com/Chia-Network",
     license="https://opensource.org/licenses/Apache-2.0",
     description="CLVM compiler.",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'License :: OSI Approved :: Apache Software License',
-        'Topic :: Security :: Cryptography',
-    ],)
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Security :: Cryptography",
+    ],
+    project_urls={
+        "Bug Reports": "https://github.com/Chia-Network/clvm_tools",
+        "Source": "https://github.com/Chia-Network/clvm_tools",
+    },
+)
