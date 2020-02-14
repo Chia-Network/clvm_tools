7655cd3c4e0dc5008e24149527c04a89c9febd63
Richard Kiss <him@richardkiss.com>
2020-02-14 11:30:58 -0800

    Add NodePath.py.

---
+++ clvm_tools/NodePath.py
@@ -0,0 +1,88 @@
+r"""
+We treat an s-expression as a binary tree, where leaf nodes are atoms and pairs
+are nodes with two children. We then number the paths as follows:
+
+              1
+             / \
+            /   \
+           /     \
+          /       \
+         /         \
+        /           \
+       2             3
+      / \           / \
+     /   \         /   \
+    4      6      5     7
+   / \    / \    / \   / \
+  8   12 10  14 9  13 11  15
+
+etc.
+
+You're probably thinking "the first two rows make sense, but why do the numbers
+do that weird thing after?" The reason has to do with making the implementation simple.
+We want a simple loop which starts with the root node, then processes bits starting with
+the least significant, moving either left or right (first or rest). So the LEAST significant
+bit controls the first branch, then the next-least the second, and so on. That leads to this
+ugly-numbered tree.
+"""
+
+from clvm import KEYWORD_TO_ATOM
+
+ARGS_KW = KEYWORD_TO_ATOM["a"]
+FIRST_KW = KEYWORD_TO_ATOM["f"]
+REST_KW = KEYWORD_TO_ATOM["r"]
+
+
+def compose_paths(path_0, path_1):
+    """
+    The binary representation of a path is a 1 (which means "stop"), followed by the
+    path as binary digits, where 0 is "left" and 1 is "right".
+
+    Look at the diagram at the top for these examples.
+
+    Example: 9 = 0b1001, so right, left, left
+    Example: 10 = 0b1010, so left, right, left
+
+    How it works: we write both numbers as binary. We ignore the terminal in path_0, since it's
+    not the terminating condition anymore. We shift path_1 enough places to OR in the rest of path_0.
+
+    Example: path_0 = 9 = 0b1001, path_1 = 10 = 0b1010.
+    Shift path_1 three places (so there is room for 0b001) to 0b1010000.
+    Then OR in 0b001 to yield 0b1010001 = 81, which is right, left, left, left, right, left.
+    """
+    mask = 1
+    temp_path = path_1
+    while temp_path > 1:
+        path_1 <<= 1
+        mask <<= 1
+        temp_path >>= 1
+
+    mask -= 1
+    path = path_1 | path_0 & mask
+    return path
+
+
+class NodePath:
+    """
+    Use 1-based paths
+    """
+
+    def __init__(self, index=1):
+        self._index = index
+
+    def as_path(self):
+        r = [ARGS_KW]
+        index = self._index
+        while index > 1:
+            r = [REST_KW if index & 1 else FIRST_KW, r]
+            index >>= 1
+        return r
+
+    def __add__(self, other_node):
+        return self.__class__(compose_paths(self._index, self.other_node._index))
+
+    def first(self):
+        return self.__class__(self._index * 2)
+
+    def rest(self):
+        return self.__class__(self._index * 2 + 1)
