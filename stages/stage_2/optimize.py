6ef8481715729cc4cfd050ed8fa88bae521e5983
Richard Kiss <him@richardkiss.com>
2020-02-17 13:52:28 -0800

    Remove optimizations related to obsolete (e) keyword.

--- stages/stage_2/optimize.py
+++ stages/stage_2/optimize.py
@@ -3,7 +3,6 @@ from clvm import KEYWORD_TO_ATOM
 
 QUOTE_KW = KEYWORD_TO_ATOM["q"]
 ARGS_KW = KEYWORD_TO_ATOM["a"]
-EVAL_KW = KEYWORD_TO_ATOM["e"]
 FIRST_KW = KEYWORD_TO_ATOM["f"]
 REST_KW = KEYWORD_TO_ATOM["r"]
 CONS_KW = KEYWORD_TO_ATOM["c"]
@@ -39,31 +38,6 @@ def constant_optimizer(r, eval):
         cost, r1 = eval(r, r.null())
         r = r.to([QUOTE_KW, r1])
     return r
-
-
-def eval_q_a_optimizer(r, eval):
-    """
-    This applies the transform
-    (e (q SEXP) (a)) => SEXP
-    """
-    if r.nullp() or not r.listp():
-        return r
-    operator = r.first()
-    if operator.listp():
-        return r
-
-    as_atom = operator.as_atom()
-    if as_atom != EVAL_KW:
-        return r
-    first_arg = r.rest().first()
-    if not first_arg.listp() or first_arg.nullp():
-        return r
-    op_2 = first_arg.first()
-    if op_2.listp() or op_2.as_atom() != QUOTE_KW:
-        return r
-    if r.rest().rest().as_python() != [[ARGS_KW]]:
-        return r
-    return first_arg.rest().first()
 
 
 def cons_q_a_optimizer(r, eval):
@@ -115,56 +89,12 @@ def sub_args(sexp, new_args):
     return sexp.to([sub_args(_, new_args) for _ in sexp.as_iter()])
 
 
-def var_change_optimizer(r, eval):
-    """
-    This applies the transform
-    (e (q (op SEXP1...)) (ARGS)) => (op SEXP1' ...) where ARGS != (a)
-    where
-    SEXP1 = (e SEXP1 (ARGS)), ... and then "children_optimizer" of this.
-    In some cases, this can result in a constant in some of the children.
-
-    If we end up needing to push the "change of variables" to only one child, keep
-    the optimization. Otherwise discard it.
-    """
-
-    if r.nullp() or not r.listp():
-        return r
-    operator = r.first()
-    if operator.listp():
-        return r
-
-    as_atom = operator.as_atom()
-    if as_atom != EVAL_KW:
-        return r
-    first_arg = r.rest().first()
-    if not first_arg.listp() or first_arg.nullp():
-        return r
-    op_2 = first_arg.first()
-    if op_2.listp() or op_2.as_atom() != QUOTE_KW:
-        return r
-    new_args = r.rest().rest().first().as_python()
-    if new_args == [ARGS_KW]:
-        # let eval_q_a_optimizer take care of this
-        return r
-
-    inner_sexp = first_arg.rest().first()
-    new_sexp = sub_args(inner_sexp, new_args)
-
-    new_operands = list(new_sexp.rest().as_iter())
-    opt_operands = [optimize_sexp(_, eval) for _ in new_operands]
-    non_constant_count = sum(1 if _.first() != QUOTE_KW else 0 for _ in opt_operands)
-    if non_constant_count < 1:
-        final_sexp = r.to([first_arg.rest().first().first()] + opt_operands)
-        return final_sexp
-    return r
-
-
 def var_change_optimizer_cons_eval(r, eval):
     """
     This applies the transform
     ((c (q (op SEXP1...)) (ARGS))) => (q RET_VAL) where ARGS != (a)
     via
-    (op (e SEXP1 (ARGS)) ...)) (ARGS)) and then "children_optimizer" of this.
+    (op ((c SEXP1 (ARGS)) ...)) (ARGS)) and then "children_optimizer" of this.
     In some cases, this can result in a constant in some of the children.
 
     If we end up needing to push the "change of variables" to only one child, keep
@@ -273,9 +203,7 @@ def optimize_sexp(r, eval):
     OPTIMIZERS = [
         cons_optimizer,
         constant_optimizer,
-        eval_q_a_optimizer,
         cons_q_a_optimizer,
-        var_change_optimizer,
         var_change_optimizer_cons_eval,
         children_optimizer,
     ]
