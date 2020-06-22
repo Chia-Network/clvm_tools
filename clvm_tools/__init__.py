e073571a94d2f8c85b493cde961dbefa125ed65c
Matthew Howard <matt@chia.net>
2020-06-22 13:02:49 +0100

    attempt to increment version number

--- clvm_tools/__init__.py
+++ clvm_tools/__init__.py
@@ -1,7 +1,7 @@
 from pkg_resources import get_distribution, DistributionNotFound
 
 try:
-    __version__ = 0.1.2
+    __version__ = "0.1.2"
 except DistributionNotFound:
     # package is not installed
     __version__ = "unknown"
