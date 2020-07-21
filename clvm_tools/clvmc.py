5c3ab045a471ebddd659e7667d01236c7daafdce
Richard Kiss <him@richardkiss.com>
2020-07-21 15:42:21 -0700

    Add `search_paths` to `compile_clvm`.

--- clvm_tools/clvmc.py
+++ clvm_tools/clvmc.py
@@ -11,16 +11,22 @@ from clvm_tools import binutils
 from stages import stage_2
 
 
-def compile_clvm(input_path, output_path):
+def compile_clvm_text(text, search_paths):
+    ir_src = reader.read_ir(text)
+    assembled_sexp = binutils.assemble_from_ir(ir_src)
+
+    input_sexp = assembled_sexp.to((assembled_sexp, []))
+    run_program = stage_2.run_program_for_search_paths(search_paths)
+    cost, result = run_program(stage_2.run, input_sexp)
+    return result
+
+
+def compile_clvm(input_path, output_path, search_paths=[]):
     if newer(input_path, output_path):
         log.info("clvmcc %s -o %s" % (input_path, output_path))
         with open(input_path) as f:
             text = f.read()
-        ir_src = reader.read_ir(text)
-        assembled_sexp = binutils.assemble_from_ir(ir_src)
-
-        input_sexp = assembled_sexp.to((assembled_sexp, []))
-        cost, result = stage_2.run_program(stage_2.run, input_sexp)
+        result = compile_clvm_text(text, search_paths)
         hex = result.as_bin().hex()
 
         with open(output_path, "w") as f:
