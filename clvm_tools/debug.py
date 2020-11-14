776fd9cf52862bf7d81c034177eab776f4759929
Richard Kiss <him@richardkiss.com>
2020-11-13 17:27:04 -0800

    Use `SExp.null()` instead of `[]`.

--- clvm_tools/debug.py
+++ clvm_tools/debug.py
@@ -105,7 +105,7 @@ def trace_to_html(invocations, disassemble):
 def build_symbol_dump(constants_lookup, run_program, path):
     compiled_lookup = {}
     for k, v in constants_lookup.items():
-        cost, v1 = run_program(v, [])
+        cost, v1 = run_program(v, SExp.null())
         compiled_lookup[sha256tree(v1).hex()] = k.decode()
     output = json.dumps(compiled_lookup)
     with open(path, "w") as f:
