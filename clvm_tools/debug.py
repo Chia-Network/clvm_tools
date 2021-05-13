0d5e123d7f0467a892dd6d797482e1208a346d1c
Richard Kiss <him@richardkiss.com>
2021-05-13 16:52:05 -0700

    Use just one instance of `sha256tree`.

--- clvm_tools/debug.py
+++ clvm_tools/debug.py
@@ -5,23 +5,12 @@ from typing import Any, Callable, List
 
 from clvm import SExp
 
+from .sha256tree import sha256tree
 
 OpCallable = Callable[[Any, "ValStackType"], int]
 
 ValStackType = List[SExp]
 OpStackType = List[OpCallable]
-
-
-def sha256tree(v):
-    pair = v.as_pair()
-    if pair:
-        left = sha256tree(pair[0])
-        right = sha256tree(pair[1])
-        s = b"\2" + left + right
-    else:
-        atom = v.as_atom()
-        s = b"\1" + atom
-    return hashlib.sha256(s).digest()
 
 
 PRELUDE = '''<html>
