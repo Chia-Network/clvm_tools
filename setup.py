e9f7ec303dd00fc9111b8c2d1e33bfa95382eac4
Kyle Altendorf <sda@fstab.net>
2024-01-23 09:23:25 -0500

    test

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "rt") as fh:
     long_description = fh.read()
 
 dependencies = [
-    "clvm>=0.9.2",
+    "clvm @ git+https://github.com/chia-network/clvm@replace_pkg_resources",
     "clvm_tools_rs>=0.1.37"
 ]
 
